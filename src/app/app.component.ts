ced19d68e11abd8a31eb8431b9a3fc2091516556
harbaz3301 <harbazbrar3223@gmail.com>
2022-09-06 16:10:37 +0530

    qwerqwerqwer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'asd';
+  serverElements = [{type: 'server', name:'test-server',content:'just a server'},{type: 'blueprint', name:'test-server',content:'just a b server'}];
+  
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 }
